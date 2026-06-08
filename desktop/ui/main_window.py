ccbaabe9f5b693706b6b3ce42a8023d5bca17d45
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2026-06-08 17:04:21 +0700

    thread shenanigans

--- desktop/ui/main_window.py
+++ desktop/ui/main_window.py
@@ -98,6 +98,9 @@ class MainWindow:
     def _update_video_frame(self, frame):
         img = Image.fromarray(frame)
         img = img.resize((320, 240), Image.Resampling.LANCZOS)
+        self.root.after(0, self._set_video_frame, img)
+
+    def _set_video_frame(self, img):
         photo = ImageTk.PhotoImage(img)
         self.video_label.img = photo
         self.video_label.config(image=photo)
